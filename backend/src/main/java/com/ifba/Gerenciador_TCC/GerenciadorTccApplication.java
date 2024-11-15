10da218c19431bed80a8ff6949b51e7e4a2ce859
Gabriel Moreira <107933147+GabrielMreira@users.noreply.github.com>
2024-11-15 09:03:09 -0300

    Inicialização do projeto

---
+++ backend/src/main/java/com/ifba/Gerenciador_TCC/GerenciadorTccApplication.java
@@ -0,0 +1,13 @@
+package com.ifba.Gerenciador_TCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorTccApplication.class, args);
+	}
+
+}
