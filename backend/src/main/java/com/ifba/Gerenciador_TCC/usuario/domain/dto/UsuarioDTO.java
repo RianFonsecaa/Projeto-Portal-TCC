e06e1fe2fa630f59a6d7f26b2deb79a7c8bd797a
Pedro Gois <114825836+Redror@users.noreply.github.com>
2025-02-07 14:59:51 -0300

    Fix(Usuario): Correção tamanho mínimo de senha
    Corrigido senha não ter verificação de tamanho mínimo. Tamanho mínimo atual de seis caracteres

--- backend/src/main/java/com/ifba/Gerenciador_TCC/usuario/domain/dto/UsuarioDTO.java
+++ backend/src/main/java/com/ifba/Gerenciador_TCC/usuario/domain/dto/UsuarioDTO.java
@@ -9,6 +9,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,15 +26,16 @@ public class UsuarioDTO {
     @NotNull(message = "Username não pode estar vazio")
     private String username;
 
-    @NotNull(message = "Senha não pode estar vazia")
-    private String senha;
-
     @NotNull(message = "Nome não pode estar vazio")
     private String nome;
 
     @NotNull(message = "Email não pode estar vazio")
     @Email(message = "Formato de email inválido")
     private String email;
+
+    @NotNull(message = "Senha não pode estar vazia")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+    private String senha;
 
     @NotNull
     @JoinColumn
