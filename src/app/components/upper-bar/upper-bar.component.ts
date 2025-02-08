6815a6d96888fcefebab9d7a53aa5806546b324e
gabriel <gabrielbritolf@gmail.com>
2025-02-08 01:58:06 -0300

    feat(DashBoard): Adicao das funcoes darkmode e menu sidebar

--- src/app/components/upper-bar/upper-bar.component.ts
+++ src/app/components/upper-bar/upper-bar.component.ts
@@ -1,25 +1,51 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-upper-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './upper-bar.component.html',
 })
 export class UpperBarComponent {
 
-  iconColor: String = '#515151'
+  @ViewChild('toggleButton') toggleButton!: ElementRef; 
+  menuOpen: boolean = false; 
 
-  toggleTheme(toggleButton: HTMLElement) {
-    if (toggleButton.classList.contains('translate-x-10')) {
-      toggleButton.classList.remove('translate-x-10');
-      toggleButton.classList.add('translate-x-0');
-      this.iconColor = '#515151'
+  constructor(private themeService: ThemeService) {}
+
+  toggle() {
+    this.themeService.toggleTheme();
+    this.updateButtonPosition();
+  }
+  get isDarkMode(): boolean {
+    return this.themeService.isDarkTheme();
+  }
+
+  private updateButtonPosition() {
+    if (this.isDarkMode) {
+      this.toggleButton.nativeElement.classList.remove('translate-x-0');
+      this.toggleButton.nativeElement.classList.add('translate-x-10');
     } else {
-      toggleButton.classList.remove('translate-x-0');
-      toggleButton.classList.add('translate-x-10');
-      this.iconColor = '#61B668'
+      this.toggleButton.nativeElement.classList.remove('translate-x-10');
+      this.toggleButton.nativeElement.classList.add('translate-x-0');
+    }
+  }
+
+  toggleMenu(event: MouseEvent) {
+    event.stopPropagation();
+    this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const menuElement = document.querySelector('nav'); 
+    const buttonElement = document.querySelector('button'); 
+    
+    if (menuElement && buttonElement && !menuElement.contains(event.target as Node) && !buttonElement.contains(event.target as Node)) {
+      this.menuOpen = false;
     }
   }
 }
