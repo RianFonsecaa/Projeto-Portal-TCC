8734b03103f259771de389ca07c33fa3e5b4b685
Guilherme <110931233+GuiSamp@users.noreply.github.com>
2025-02-24 01:06:50 -0300

    refactor(Quadro de demandas): Reestilização dos campos do modal de adição de demandas

--- src/app/components/modais/modal-demandas-orientador/modal-demandas-orientador.component.ts
+++ src/app/components/modais/modal-demandas-orientador/modal-demandas-orientador.component.ts
@@ -3,14 +3,31 @@ import { ModalService } from '../../../services/modal.service';
 import { ThemeService } from '../../../services/theme.service';
 import { NgClass } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-modal-demandas-orientador',
   standalone: true,
-  imports: [NgClass, ReactiveFormsModule, FormsModule],
+  imports: [NgClass, ReactiveFormsModule, FormsModule, NgFor, NgIf],
   templateUrl: './modal-demandas-orientador.component.html',
 })
 export class ModalDemandasOrientador {
+  nomesDisponiveis = [
+    { nome: 'Pesquisa', selected: false },
+    { nome: 'Desenvolvimento', selected: false },
+    { nome: 'Leitura', selected: false },
+    { nome: 'Leitura', selected: false },
+    { nome: 'Leitura', selected: false },
+    { nome: 'Leitura', selected: false },
+    { nome: 'Leitura', selected: false },
+    { nome: 'Leitura', selected: false },
+    { nome: 'Leitura', selected: false },
+  ];
+
+  dropdownVisible: boolean = false;
+  isRotated: boolean = false; // Variável para controlar a rotação
+  nomesSelecionados: string[] = [];
+
   demandasOrientador = {
     titulo: '',
     descricao: '',
@@ -34,4 +51,14 @@ export class ModalDemandasOrientador {
     this.modalService.abrir(nomeModal);
   }
 
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+    this.isRotated = !this.isRotated; // Alterna a rotação do ícone
+  }
+
+  updateLista() {
+    this.nomesSelecionados = this.nomesDisponiveis
+      .filter(nome => nome.selected)
+      .map(nome => nome.nome);
+  }
 }
