a651424d1bccd37836cdfde953122590bf3d7ec4
RianFonsecaa <rian.sfonsecaa@gmail.com>
2025-03-10 16:28:54 -0300

    revert: reverter para o commit f832fe6efc8cecb0b6324cd15cb648531d70c980

--- backend/target/generated-sources/annotations/com/ifba/Gerenciador_TCC/usuario/mapper/UsuarioMapperImpl.java
+++ backend/target/generated-sources/annotations/com/ifba/Gerenciador_TCC/usuario/mapper/UsuarioMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-10T15:02:21-0300",
+    date = "2025-03-10T16:25:58-0300",
     comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.41.0.z20250213-2037, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
