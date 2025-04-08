465c8d6a7301f31e164296a89d8e4c5324e43742
YuriPepezin <pepeyuri2003@gmail.com>
2025-04-08 14:16:47 -0300

    refactor(backend): refatoraçao no método editarTarefa e nos modulo email

---
+++ backend/target/generated-sources/annotations/com/ifba/Gerenciador_TCC/projeto/mapper/ProjetoMapperImpl.java
@@ -0,0 +1,55 @@
+package com.ifba.Gerenciador_TCC.projeto.mapper;
+
+import com.ifba.Gerenciador_TCC.projeto.domain.dto.ProjetoDTO;
+import com.ifba.Gerenciador_TCC.projeto.domain.entity.Projeto;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-04-08T14:15:17-0300",
+    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
+)
+@Component
+public class ProjetoMapperImpl extends ProjetoMapper {
+
+    @Override
+    public Projeto dtoToProjeto(ProjetoDTO projetoDTO) {
+        if ( projetoDTO == null ) {
+            return null;
+        }
+
+        Projeto.ProjetoBuilder projeto = Projeto.builder();
+
+        projeto.dataCadastro( projetoDTO.getDataCadastro() );
+        projeto.descricaoProjeto( projetoDTO.getDescricaoProjeto() );
+        projeto.horasOrientacao( projetoDTO.getHorasOrientacao() );
+        projeto.id( projetoDTO.getId() );
+        projeto.progresso( projetoDTO.getProgresso() );
+        projeto.tipoCurso( projetoDTO.getTipoCurso() );
+        projeto.tipoProjeto( projetoDTO.getTipoProjeto() );
+        projeto.tituloProjeto( projetoDTO.getTituloProjeto() );
+
+        return projeto.build();
+    }
+
+    @Override
+    public ProjetoDTO projetoToDTO(Projeto projeto) {
+        if ( projeto == null ) {
+            return null;
+        }
+
+        ProjetoDTO.ProjetoDTOBuilder projetoDTO = ProjetoDTO.builder();
+
+        projetoDTO.dataCadastro( projeto.getDataCadastro() );
+        projetoDTO.descricaoProjeto( projeto.getDescricaoProjeto() );
+        projetoDTO.horasOrientacao( projeto.getHorasOrientacao() );
+        projetoDTO.id( projeto.getId() );
+        projetoDTO.progresso( projeto.getProgresso() );
+        projetoDTO.tipoCurso( projeto.getTipoCurso() );
+        projetoDTO.tipoProjeto( projeto.getTipoProjeto() );
+        projetoDTO.tituloProjeto( projeto.getTituloProjeto() );
+
+        return projetoDTO.build();
+    }
+}
---
+++ backend/target/generated-sources/annotations/com/ifba/Gerenciador_TCC/usuario/mapper/UsuarioMapperImpl.java
@@ -0,0 +1,51 @@
+package com.ifba.Gerenciador_TCC.usuario.mapper;
+
+import com.ifba.Gerenciador_TCC.usuario.domain.dto.UsuarioDTO;
+import com.ifba.Gerenciador_TCC.usuario.domain.entity.Usuario;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-04-08T14:15:16-0300",
+    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
+)
+@Component
+public class UsuarioMapperImpl extends UsuarioMapper {
+
+    @Override
+    public Usuario dtoToUsuario(UsuarioDTO usuarioDTO) {
+        if ( usuarioDTO == null ) {
+            return null;
+        }
+
+        Usuario.UsuarioBuilder usuario = Usuario.builder();
+
+        usuario.email( usuarioDTO.getEmail() );
+        usuario.id( usuarioDTO.getId() );
+        usuario.nome( usuarioDTO.getNome() );
+        usuario.senha( usuarioDTO.getSenha() );
+        usuario.telefone( usuarioDTO.getTelefone() );
+        usuario.tipoUsuario( usuarioDTO.getTipoUsuario() );
+
+        return usuario.build();
+    }
+
+    @Override
+    public UsuarioDTO usuarioToDTO(Usuario usuario) {
+        if ( usuario == null ) {
+            return null;
+        }
+
+        UsuarioDTO.UsuarioDTOBuilder usuarioDTO = UsuarioDTO.builder();
+
+        usuarioDTO.email( usuario.getEmail() );
+        usuarioDTO.id( usuario.getId() );
+        usuarioDTO.nome( usuario.getNome() );
+        usuarioDTO.senha( usuario.getSenha() );
+        usuarioDTO.telefone( usuario.getTelefone() );
+        usuarioDTO.tipoUsuario( usuario.getTipoUsuario() );
+
+        return usuarioDTO.build();
+    }
+}
