13b04ace5848384a63dc69d07a9fb9bbab119df5
Árlei Nóbrega Oliveira <114442255+Arlei10@users.noreply.github.com>
2025-04-08 09:59:03 -0300

    feat:backend-ajustes-banco-documentos

--- backend/target/generated-sources/annotations/com/ifba/Gerenciador_TCC/projeto/mapper/ProjetoMapperImpl.java
+++ backend/target/generated-sources/annotations/com/ifba/Gerenciador_TCC/projeto/mapper/ProjetoMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-08T09:42:30-0300",
+    date = "2025-04-08T09:53:51-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
